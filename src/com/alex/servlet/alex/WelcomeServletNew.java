db83de6f115f32dbeb678afd3cd844bdf6f0b01e
Alex Trzeciak <trebek08@gmail.com>
2017-08-13 16:17:22 -0700

    java spring demo

---
+++ src/com/alex/servlet/alex/WelcomeServletNew.java
@@ -0,0 +1,51 @@
+package com.alex.servlet.alex;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServletNew extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServletNew() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1. Create thedata (model) and then add it to the request object 
+		String welcomeMessage[] = {"Hello!", "Welcome to the Spring course."}; 
+		request.setAttribute("myWelcomeMessage", welcomeMessage);
+		
+		//2. Retrieve request dispatcher
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("welcome.jsp");
+		
+		//3. Forward request to the view 
+		requestDispatcher.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
